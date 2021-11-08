542910772d6fa6d2f4f9e2ecc689d84fa596649c
kazmi017 <kazmi82033@gmail.com>
2021-11-09 00:46:34 +0500

    Adopted New Design(d1)

---
+++ job_hunter_fend_js/src/components/home/Home.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Home.scss'
+class Home extends Component {
+    render() {
+        return (
+            <div className="main">
+                <div className="head">
+                    <div className="logo">logo</div>
+                     <div className="menus">menus</div>
+                    <div className="join">join</div>
+                </div>
+      
+    </div>
+        );
+    }
+}
+
+export default Home;
