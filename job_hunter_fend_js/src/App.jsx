0b712936c28586d2790b60257f1bd55fd51c8704
kazmi017 <kazmi82033@gmail.com>
2021-11-15 03:50:34 +0500

    comit

--- job_hunter_fend_js/src/App.jsx
+++ job_hunter_fend_js/src/App.jsx
@@ -1,11 +1,14 @@
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import './App.scss';
 import Home from './components/home/Home';
+import Join from './components/login/Join.jsx'
 function App() {
-  return (
-    <div>
-      <Home/>
-    </div>
-  );
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/join" element={<Join/>} />
+    </Routes>
+  </Router>
 }
 
 export default App;
