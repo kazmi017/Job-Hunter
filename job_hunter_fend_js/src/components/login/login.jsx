44381abf01dcd4906f36bccedb4d35eab9a7e635
kazmi017 <kazmi82033@gmail.com>
2021-12-08 13:34:41 +0500

    redux added

--- job_hunter_fend_js/src/components/login/login.jsx
+++ job_hunter_fend_js/src/components/login/login.jsx
@@ -1,38 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../features/userSlice";
 import loginImg from "../../login.svg";
 
-export class Login extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
-  render() {
+export function Login(props) {
+
+   const [email,setEmail]= useState("");
+   const [password,setPassword]= useState("");
+
+   const dispatch=useDispatch();
+
+   const URL='https://jobhunter5758.000webhostapp.com/login.php'
+
+
+  const handleSubmit=(e)=>{
+
+    e.preventDefault();
+
+    dispatch(login({
+      isloggedIn:true,
+    }));
+   };
+
+
+
     return (
-      <div className="base-container" ref={this.props.containerRef}>
+      <div className="base-container" ref={props.containerRef}>
         <div className="header">Login</div>
+        
+        <form onSubmit={(e) => handleSubmit(e)}> 
         <div className="content">
+        
           <div className="image">
             <img src={loginImg} />
           </div>
+          
           <div className="form">
             <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" placeholder="Email" 
+              onChange={(e)=> setEmail({email: e.target.value}) }/>
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" />
+              <input type="password" name="password" placeholder="password"
+              onChange={(e)=> setPassword({password:e.target.value})} />
+              
             </div>
           </div>
         </div>
         <div className="footer">
-          <a href="/dashboard">
-          <button type="button" className="btn">
+          <button type="submit" className="btn">
             Login
           </button>
-          </a>
         </div>
+        </form>
       </div>
     );
   }
-}
+
